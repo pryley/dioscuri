fe8b63d6556f04c87f4f8076c170e1a64c980a94
Paul Ryley <paul@geminilabs.io>
2020-10-27 13:17:34 -0600

    update Valet driver for multisite

--- LocalValetDriver.php
+++ LocalValetDriver.php
@@ -12,6 +12,7 @@ class LocalValetDriver extends BasicValetDriver
      */
     public function frontControllerPath($sitePath, $siteName, $uri)
     {
+        $uri = $this->rewriteMultisite($sitePath, $uri);
         $_SERVER['PHP_SELF'] = $uri;
         $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'];
         if (0 === strpos($uri, '/wp/')) {
@@ -32,6 +33,7 @@ class LocalValetDriver extends BasicValetDriver
      */
     public function isStaticFile($sitePath, $siteName, $uri)
     {
+        $uri = $this->rewriteMultisite($sitePath, $uri);
         $staticFilePath = $sitePath.'/public'.$uri;
         if ($this->isActualFile($staticFilePath)) {
             return $staticFilePath;
@@ -67,6 +69,36 @@ class LocalValetDriver extends BasicValetDriver
         if ('/wp/wp-admin' == substr($uri, -1 * strlen('/wp/wp-admin'))) {
             header('Location: '.$uri.'/');
             die;
+        }
+        return $uri;
+    }
+
+    /**
+     * Determine if the application is Multisite.
+     *
+     * @param $sitePath
+     * @return bool
+     */
+    protected function isMultisite($sitePath)
+    {
+        $app = file_get_contents($sitePath.'/env.php');
+        return (bool) preg_match("/define\(\s*('|\")MULTISITE\\1\s*,\s*true\s*\)/mi", $app);
+    }
+
+    /**
+     * Imitate the rewrite rules for a multisite .htaccess.
+     *
+     * @param $sitePath
+     * @param $uri
+     * @return string
+     */
+    protected function rewriteMultisite($sitePath, $uri)
+    {
+        if (!$this->isMultisite($sitePath)) {
+            return $uri;
+        }
+        if (preg_match('#^(/[^/]+)?(?!/wp-json)(/wp-.*)#', $uri, $matches) || preg_match('#^(/[^/]+)?(/.*\.php)#', $uri, $matches)) {
+            return "/wp{$matches[2]}";
         }
         return $uri;
     }
